12a93827037fd327efc70ef7cd44ffaab4fe9499
rachna <rachnaoem07@gmail.com>
2026-03-27 21:53:28 +0700

    telegram bot

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,8 @@
 import dotenv from "dotenv";
+dotenv.config();
 import connectDB from "./src/config/db.js";
 import app from "./src/app.js";
 // import createAdmin from "./src/services/seedAdmin.js";
-
-dotenv.config();
 
 // Database
 connectDB();
