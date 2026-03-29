fd0a75d7f6eb8de8a76f7c0cb6804b41ee500830
Haysansan <haysan.eang@gmail.com>
2026-03-29 17:32:55 +0700

    fix: add api changes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,8 +3,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-});
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+});;
 
 // Attach token to every request
 api.interceptors.request.use((req) => {
