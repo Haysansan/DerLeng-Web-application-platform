6442ce8f806b19913f911b8c660234fd8b918ec7
Haysansan <haysan.eang@gmail.com>
2026-04-01 04:35:22 +0700

    fix: call api from api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@
 // frontend/src/services/api.js
 import axios from "axios";
 
-const API = axios.create({
+const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000"
 });
 
